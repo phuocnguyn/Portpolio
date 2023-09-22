29babd08fa872a5233392a8799b0f7950fc1ee3d
Huỳnh Phước Nguyên <huynhphuocnguyen2412@gmail.com>
2023-09-23 02:37:21 +0700

    reponsive web

--- js/app.js
+++ js/app.js
@@ -1,11 +1,23 @@
-$(document).ready(function()
-{
-    $('.certificate-list').slick({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear'
-      });
-}
-);
+$(document).ready(function () {
+   
+ $('.certificate-list').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    infinite: false,
+    asNavFor: '.certificate-image-list'
+  });
+  $('.certificate-image-list').slick({
+   
+    slidesToShow: 3,
+   
+    asNavFor: '.certificate-list',
+    infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1
+  
+  });
+          
+});
+
