4b9084b33083e2475fdf2b15ec56625d0c57cdc0
phuocnguyn <huynhphuocnguyen2412@gmail.com>
2023-09-23 21:58:09 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,23 +1,5 @@
-$(document).ready(function () {
-   
- $('.certificate-list').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    infinite: false,
-    asNavFor: '.certificate-image-list'
-  });
-  $('.certificate-image-list').slick({
-   
-    slidesToShow: 3,
-   
-    asNavFor: '.certificate-list',
-    infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 1
-  
-  });
-          
+<script>
+$(document).ready(function() {
+    $('.pgwSlider').pgwSlider();
 });
-
+</script>
