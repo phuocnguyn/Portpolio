0e2d2b6bc4fc0111aee2b52186dd1c6b1f1add65
Phước Nguyên tới chơi :D <ttcenter@TTCenters-MacBook-Pro.local>
2023-12-02 20:31:39 +0700

    slider

--- js/pgwslider.js
+++ js/pgwslider.js
@@ -1,12 +1,4 @@
-/**
- * PgwSlider - Version 2.3
- *
- * Copyright 2014, Jonathan M. Piat
- * http://pgwjs.com - http://pagawa.com
- * 
- * Released under the GNU GPLv3 license - http://opensource.org/licenses/gpl-3.0
- */
-;(function($){
+(function($){
     $.fn.pgwSlider = function(options) {
 
         var defaults = {
